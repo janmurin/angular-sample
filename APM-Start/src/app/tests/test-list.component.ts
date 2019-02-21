00d8b0c81f856036da298037f381c67ad0029655
janmurin2 <janmurin2@gmail.com>
2019-02-21 17:26:23 +0100

    databinding

--- APM-Start/src/app/tests/test-list.component.ts
+++ APM-Start/src/app/tests/test-list.component.ts
@@ -7,9 +7,15 @@ import {Component} from '@angular/core';
 })
 export class TestListComponent {
   pageTitle = 'Tests List with typescript';
+  showImage = true;
+  listFilter = 'test';
   tests: any[] = [
-    {name: 'first test', description: 'first description'},
-    {name: 'second test', description: 'second description'},
-    {name: 'third test', description: 'third description'}
+    {name: 'first test', description: 'first description', imgUrl: 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'},
+    {name: 'second test', description: 'second description', imgUrl: 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'},
+    {name: 'third test', description: 'third description', imgUrl: 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'}
   ];
+
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
 }
